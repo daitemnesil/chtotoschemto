aad79b13f6a861f81558de6a72f175193cfc7bef
Asel <91185593+daitemnesil@users.noreply.github.com>
2021-12-18 20:22:05 +0300

    Update main_bot_tg.py

--- main_bot_tg.py
+++ main_bot_tg.py
@@ -6,6 +6,7 @@ import constants
 from telebot import types
 from main import get_weather
 from telebot import types
+import asyncio
 import requests
 import datetime
 from urllib3.util import url
@@ -17,27 +18,22 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
     InlineKeyboardButton
 import markup as nav
 import random
-import os
 
 TOKEN = '5069121340:AAEFDLJLYe2HmXmBSTMhmPaj_aTI-rB0k7c'
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot) #создали объект класса дистпачер, он нужен для создания хэндлеров
 
 """Функция, запускающая начало работы и высылающая сообщение-приветсвие.
 Открывает перед пользователем главное меню."""
-
-
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start']) #ожидает сообщения-команды
 async def command_start(message: types.Message):
     await bot.send_message(message.from_user.id, 'Hello {0.first_name}'.format(message.from_user),
-                           reply_markup=nav.mainMenu)
-
+                           reply_markup=nav.mainMenu) #автоматическое сообщение-приветсвие
+                           # reply_markup перенаправляет пользователя, в данном случае в главное меню
 
 """Функция, отвечающая за то, какую информацию выдать в зависимости от того,
 на какую из предложенных кнопок нажал пользователь. 
 Прописываются все исходы событий и выдают информацию исходя из назначения кнопки."""
-
-
 @dp.message_handler()
 async def bot_message(message: types.Message):
     """Исходя из нажатой кнопки отправляется определнный текст"""
@@ -47,19 +43,79 @@ async def bot_message(message: types.Message):
         """random.randint генерирует рандомное число в заданном диапазоне"""
         await bot.send_message(message.from_user.id, 'Ставьте ' + str(random.randint(8, 10)))
     elif message.text == 'Погода':
-        await bot.send_message(message.from_user.id, 'Погода в каком городе вас интересует?')
+        await bot.send_message(message.from_user.id, 'Выберите город'.format(message.from_user),reply_markup=nav.cityMenu)
+    elif message.text == 'Москва' or message.text == 'Санкт-Петербург' or message.text == 'Новосибирск' or message.text == 'Казань'\
+        or message.text == 'Нижний Новгород' or message.text == 'Челябинск' or message.text == 'Самара'\
+            or message.text == 'Омкс' or message.text == 'Ростов-на-Дону' or message.text == 'Уфа' or message.text == 'Пермь'\
+            or message.text == 'Красноярск' or message.text == 'Воронеж' or message.text == 'Волгоград':
+
+        async def mainw():
+            city=message.text
+            await get_weather(city, open_weather_token)
+        async def get_weather(city, open_weather_token):
+            # библиотека
+            code_to_smile = {
+                "Clear": "Ясно \U00002600",
+                "Clouds": "Облачно \U00002601",
+                "Rain": "Дождливо \U00002614",
+                "Drizzle": "Дождливо \U00002614",
+                "Thundershtorm": "Гроза \U000026A1",
+                "Snow": "Снег \U0001F328"
+            }
+            try:  # запрос на openweathermap для получения данных
+                r = requests.get(
+                    f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
+                )
+                data = r.json()
+                # pprint(data)
+
+                city = data["name"]
+                cur_weather = data["main"]["temp"]
+
+                weather_description = data["weather"][0]["main"]
+                if weather_description in code_to_smile:
+                    wd = code_to_smile[weather_description]
+                else:
+                    wd = "\U00000001"
+
+                humidity = data["main"]["humidity"]  # значение влажности
+                pressure = data["main"]["pressure"]  # значение давления
+                wind = data["wind"]["speed"]  # значение скорости ветра
+                sunrise = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])  # время рассвета
+                sunset = datetime.datetime.fromtimestamp(data["sys"]["sunset"])  # ремя заката
+
+                # Возвращаются необходимые данные, задается оформление
+                await message.reply(f"----{datetime.datetime.now().strftime('%d-%m-%y %H:%M')}----\n"
+                             f"Погода в городе {city} на текущий момент:\nТемпература: {cur_weather}°С {wd}\n"
+                             f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст.\n"
+                             f"Скорость ветра: {wind}м/с\nВремя рассвета: {sunrise}\nВремя заката: {sunset}\n"
+                             f"\n"
+                             f"Защити себя от коронавируса - ПОСТАВЬ ПРИВИВКУ\n"
+                             f"Госуслуги - проще, чем кажется\n"
+                             f"Не болейте!")
+            except:
+                await message.reply('Ошибка! Проверьте название введенного города.')
+                handle = None
+
+        if __name__ == "__main__":
+            await mainw()
+
     elif message.text == 'Куда пойти?':
         """reply_markup перенаправляет пользователя в новое место, в данном случае в доп меню"""
-        await bot.send_message(message.from_user.id, 'Вот:', reply_markup=nav.otherMenu)
-        await message.reply(f"Скоро функционал данного бота будет раширен.\n"
-                            f"Следите за обновлениями!\n")
+        await bot.send_message(message.from_user.id, 'Выберите:', reply_markup=nav.otherMenu)
+    elif message.text == 'Еда':
+        await bot.send_message(message.from_user.id, 'Тебе бы на диету, но ладно...')
+    elif message.text == 'Поглазеть':
+        await bot.send_message(message.from_user.id, 'Надо же, удивительно, не вариант с едой...')
+    elif message.text == 'Кино':
+        await bot.send_message(message.from_user.id, 'Хочешь спойлер? Ладно-ладно, не буду...')
     elif message.text == 'Какое ты животное?':
         await bot.send_message(message.from_user.id, 'Какое ты животное? Правда хочешь узнать?!')
         await message.reply(f"Скоро функционал данного бота будет раширен.\n"
                             f"Следите за обновлениями!\n")
     elif message.text == 'Вода':
-        await message.reply(f"----Рыбы----\n"
-                            f"Стихия - Вода\n"
+        await message.reply(f"----Рыбы----\n" #\n переносит на новую строку
+                            f"Стихия - Вода\n" #f - начало новой строки
                             f"Планета - Нептун\n"
                             f"Рыбы – изменчивые и непрактичные. Хорошо развитое воображение и богатый внутренний мир\n"
                             f"часто уводят Рыб в страну грез. Фантазии и мечты настолько заполняют жизнь, что порой\n"
@@ -149,7 +205,7 @@ async def bot_message(message: types.Message):
                             f"Cтрелец - мечтательный и бесстрашный. Стрелец - путешественник и первооткрыватель,\n"
                             f"он отличается любознательностью и жаждой знаний,\n"
                             f"однако порой вспыльчив и чересчур эмоционален.\n")
-    elif message.text == 'Назад':
+    elif message.text == 'Назад': # возвращает пользователя в главное меню
         await bot.send_message(message.from_user.id, 'ну вы и дурак...', reply_markup=nav.mainMenu)
 
 if __name__ == '__main__':
