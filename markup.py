1185d2c5ca4abf4db5149db5357a7127600fceb2
Asel <91185593+daitemnesil@users.noreply.github.com>
2021-12-19 14:48:28 +0300

    Update markup.py

--- markup.py
+++ markup.py
@@ -6,34 +6,49 @@ btnMain = KeyboardButton('Главное меню')
 # --- Main menu ---
 btnPog = KeyboardButton('Погода') #кнопка для того, чтобы узнать погоду в каком-то городе
 btnWtg = KeyboardButton('Куда пойти?') #кнопка, перенаправляющая в доп меню с вариантами куда можно сходить
-btnCat = KeyboardButton('Какое ты животное?')
 btnRand = KeyboardButton('Какую оценку поставить?')
-btnTaro = KeyboardButton('Узнай...')
-btns1 = KeyboardButton('Вода')
-btns2 = KeyboardButton('Земля')
-btns3 = KeyboardButton('Воздух')
-btns4 = KeyboardButton('Огонь')
+btnPr = KeyboardButton('Приколюхи')
+btnKR = KeyboardButton('Криминальная Россия')
 """Добавление всех вышезаданных точек. 
 resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
-mainMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnPog, btnTaro, btnWtg, btnCat, btnRand, btns1, btns2, btns3, btns4)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnPog, btnWtg, btnRand, btnPr, btnKR)
 
-"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Куда пойти?"""
+"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Куда пойти?'"""
 # ---Other menu---
 btnFood = KeyboardButton('Еда')
 btnAttr = KeyboardButton('Поглазеть')
 btnCinema = KeyboardButton('Кино')
-btnBack = KeyboardButton('Назад')
+btnBack = KeyboardButton('Главное меню')
 """Добавление всех вышезаданных точек. 
 resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 otherMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFood, btnAttr, btnCinema, btnBack)
 
+"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Приколюхи'"""
+# ---Prikolukhi menu---
+btnZZ = KeyboardButton('Гороскоп')
+btnCat = KeyboardButton('Какое ты животное?')
+btnTaro = KeyboardButton('Узнай...')
+btnBack1 = KeyboardButton('Главное меню')
+prikMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnZZ, btnCat, btnTaro, btnBack1)
+
+"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Гороскоп'"""
+# ---Goroskop menu---
+btns1 = KeyboardButton('Вода')
+btns2 = KeyboardButton('Земля')
+btns3 = KeyboardButton('Воздух')
+btns4 = KeyboardButton('Огонь')
+btnBack2 = KeyboardButton('Назад')
+gorMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btns1, btns2, btns3, btns4, btnBack2)
+
+"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Узнай...'"""
 # ---Gadanie menu---
 btnf1 = KeyboardButton('Жизненный настрой')
 btnf2 = KeyboardButton('Комплексы')
 btnf3 = KeyboardButton('Бизнесмен')
-btnBack = KeyboardButton('Назад')
-gadMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnf1, btnf2, btnf3, btnBack)
+btnBack3 = KeyboardButton('Назад')
+gadMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnf1, btnf2, btnf3, btnBack3)
 
+"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Погода'"""
 # ---City---
 btnM = KeyboardButton('Москва')
 btnSp = KeyboardButton('Санкт-Петербург')
@@ -50,10 +65,15 @@ btnKr = KeyboardButton('Красноярск')
 btnV = KeyboardButton('Воронеж')
 btnPer = KeyboardButton('Пермь')
 btnVol = KeyboardButton('Волгоград')
-btnBack = KeyboardButton('Назад')
+btnBack4 = KeyboardButton('Главное меню')
 cityMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnM, btnSp, btnNov, btnEkb, btnKaz, btnNN, btnCh, btnSam,
-                                                         btnOm, btnRND, btnUfa, btnKr, btnV, btnPer, btnVol, btnBack)
+                                                         btnOm, btnRND, btnUfa, btnKr, btnV, btnPer, btnVol, btnBack4)
 
-
-
+"""Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Криминальная Россия'"""
+# ---Krimenalnaya Rossia---
+btnkr95 = KeyboardButton('1995')
+btnkr97 = KeyboardButton('1997')
+btnkr98 = KeyboardButton('1998')
+btnBack5 = KeyboardButton('Главное меню')
+krimMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnkr95, btnkr97, btnkr98, btnBack5)
 
