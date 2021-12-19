409ca5384b54bec4c1d93470df89ecaba347d946
Asel <91185593+daitemnesil@users.noreply.github.com>
2021-12-19 14:57:05 +0300

    Update markup.py

--- markup.py
+++ markup.py
@@ -6,11 +6,10 @@ btnMain = KeyboardButton('Главное меню')
 # --- Main menu ---
 btnPog = KeyboardButton('Погода') #кнопка для того, чтобы узнать погоду в каком-то городе
 btnWtg = KeyboardButton('Куда пойти?') #кнопка, перенаправляющая в доп меню с вариантами куда можно сходить
-btnRand = KeyboardButton('Какую оценку поставить?')
-btnPr = KeyboardButton('Приколюхи')
-btnKR = KeyboardButton('Криминальная Россия')
-"""Добавление всех вышезаданных точек. 
-resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
+btnRand = KeyboardButton('Какую оценку поставить?') #кнопка, выполняющая запрос
+btnPr = KeyboardButton('Приколюхи') #кнопка открывающая меню с разными прикольными функциями
+btnKR = KeyboardButton('Криминальная Россия') #Кнопка, открывающая меню с выпусками Криминальной Росии
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 mainMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnPog, btnWtg, btnRand, btnPr, btnKR)
 
 """Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Куда пойти?'"""
@@ -19,33 +18,35 @@ btnFood = KeyboardButton('Еда')
 btnAttr = KeyboardButton('Поглазеть')
 btnCinema = KeyboardButton('Кино')
 btnBack = KeyboardButton('Главное меню')
-"""Добавление всех вышезаданных точек. 
-resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 otherMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFood, btnAttr, btnCinema, btnBack)
 
 """Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Приколюхи'"""
 # ---Prikolukhi menu---
-btnZZ = KeyboardButton('Гороскоп')
-btnCat = KeyboardButton('Какое ты животное?')
-btnTaro = KeyboardButton('Узнай...')
-btnBack1 = KeyboardButton('Главное меню')
+btnZZ = KeyboardButton('Гороскоп') #при нажатии на эту кнопку откроется меню с гороскопом
+btnCat = KeyboardButton('Какое ты животное?') #при нажатии на эту кнопку рандомайзер выдаст картинку
+btnTaro = KeyboardButton('Узнай...') #при нажатии на эту кнопку откроется меню с мини-тестами
+btnBack1 = KeyboardButton('Главное меню') #при нажатии на эту кнопку пользователь вернется назад в Главное меню
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 prikMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnZZ, btnCat, btnTaro, btnBack1)
 
 """Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Гороскоп'"""
 # ---Goroskop menu---
-btns1 = KeyboardButton('Вода')
-btns2 = KeyboardButton('Земля')
-btns3 = KeyboardButton('Воздух')
-btns4 = KeyboardButton('Огонь')
-btnBack2 = KeyboardButton('Назад')
+btns1 = KeyboardButton('Вода') #при нажатии на эту кнопку бот отправит гороскоп для водных знаков
+btns2 = KeyboardButton('Земля') #при нажатии на эту кнопку бот отправит гороскоп для земных знаков
+btns3 = KeyboardButton('Воздух') #при нажатии на эту кнопку бот отправит гороскоп для воздушных знаков
+btns4 = KeyboardButton('Огонь') #при нажатии на эту кнопку бот отправит гороскоп для огненных знаков
+btnBack2 = KeyboardButton('Назад') #при нажатии на эту кнопку пользователь вернется назад в меню Приколюхи
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 gorMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btns1, btns2, btns3, btns4, btnBack2)
 
 """Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Узнай...'"""
 # ---Gadanie menu---
-btnf1 = KeyboardButton('Жизненный настрой')
-btnf2 = KeyboardButton('Комплексы')
-btnf3 = KeyboardButton('Бизнесмен')
-btnBack3 = KeyboardButton('Назад')
+btnf1 = KeyboardButton('Жизненный настрой') #при нажатии на эту кнопку появится мини-тест
+btnf2 = KeyboardButton('Комплексы') #при нажатии на эту кнопку появится мини-тест
+btnf3 = KeyboardButton('Бизнесмен') #при нажатии на эту кнопку появится мини-тест
+btnBack3 = KeyboardButton('Назад') #при нажатии наэту кнопку пользователь вернется назад в меню Приколюхи
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 gadMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnf1, btnf2, btnf3, btnBack3)
 
 """Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Погода'"""
@@ -65,15 +66,17 @@ btnKr = KeyboardButton('Красноярск')
 btnV = KeyboardButton('Воронеж')
 btnPer = KeyboardButton('Пермь')
 btnVol = KeyboardButton('Волгоград')
-btnBack4 = KeyboardButton('Главное меню')
+btnBack4 = KeyboardButton('Главное меню') #при нажатии на эту кнопку пользователь вернется назад в Главное меню
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 cityMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnM, btnSp, btnNov, btnEkb, btnKaz, btnNN, btnCh, btnSam,
                                                          btnOm, btnRND, btnUfa, btnKr, btnV, btnPer, btnVol, btnBack4)
 
 """Кнопки дополнительного меню, открывающиеся если пользователь нажал на кнопку 'Криминальная Россия'"""
 # ---Krimenalnaya Rossia---
-btnkr95 = KeyboardButton('1995')
-btnkr97 = KeyboardButton('1997')
-btnkr98 = KeyboardButton('1998')
-btnBack5 = KeyboardButton('Главное меню')
+btnkr95 = KeyboardButton('1995') #при нажатии на эту кнопку бот выдаст рандомную серии 1995 года
+btnkr97 = KeyboardButton('1997') #при нажатии на эту кнопку бот выдаст рандомную серии 1997 года
+btnkr98 = KeyboardButton('1998') #при нажатии на эту кнопку бот выдаст рандомную серии 1998 года
+btnBack5 = KeyboardButton('Главное меню') #при нажатии на эту кнопку пользователь вернется назад в Главное меню
+"""Добавление всех вышезаданных точек. resize_keyboard = True  - отвечает за то, чтобы кнопки подстраивались по размеру"""
 krimMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnkr95, btnkr97, btnkr98, btnBack5)
 
